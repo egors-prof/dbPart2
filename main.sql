a474bb11334a8af40b49c1cc41493b13fda3f957
egors-prof <egorsenatorovprof@gmail.com>
2025-09-10 15:06:20 -0700

    formatting file

--- main.sql
+++ main.sql
@@ -1,42 +1,42 @@
 --1--
-select model,speed,hd from PC
-where price<500;
+SELECT model,speed,hd FROM PC
+WHERE price<500;
 --2--
-select maker from Product where type='Printer'group by maker
+SELECT maker FROM Product WHERE type='Printer'GROUP BY maker
 --3--
-select model,ram,screen from Laptop where price>1000;
+SELECT model,ram,screen FROM  Laptop WHERE price>1000;
 --4--
-select*from Printer where color='y';
+SELECT *FROM  Printer WHERE color='y';
 --5--
-Select model,speed,hd from PC where price<600 and (cd='12x' or cd='24x');
+SELECT model,speed,hd FROM  PC WHERE price<600 AND (cd='12x' or cd='24x');
 ---6---
-Select maker, speed  from Product inner join Laptop on Product.model = Laptop.model
-where hd >= 10
+SELECT maker, speed  FROM  Product INNER JOIN Laptop ON Product.model = Laptop.model
+WHERE hd >= 10
 ---7---
-select Laptop.model,Laptop.price from Laptop inner join Product on Laptop.model=Product.model
-where maker ='B'
-union
-select Pc.model,Pc.price from Pc inner join Product on Pc.model=Product.model
-where maker ='B'
-union
-select Printer.model,Printer.price from Printer inner join Product on Printer.model=Product.model
-where maker ='B'
+SELECT Laptop.model,Laptop.price FROM Laptop INNER JOIN Product ON Laptop.model=Product.model
+WHERE maker ='B'
+UNION
+SELECT Pc.model,Pc.price FROM Pc INNER JOIN Product ON Pc.model=Product.model
+WHERE maker ='B'
+UNION
+SELECT Printer.model,Printer.price FROM Printer INNER JOIN Product ON Printer.model=Product.model
+WHERE maker ='B'
 ---8---
-select maker  from Product where type ='Pc'
-except
-select maker from Product where type ='Laptop'
+SELECT maker FROM Product WHERE type ='Pc'
+EXCEPT
+SELECT maker FROM Product WHERE type ='Laptop'
 ---9---
-Select maker from Product join Pc on Product.model=Pc.model where speed>=450 group by maker
+SELECT maker FROM Product JOIN Pc ON Product.model=Pc.model WHERE speed>=450 GROUP BY maker
 ---10---
-select model,price from Printer where price =(select max(price)from Printer)
+SELECT model,price FROM Printer WHERE price =(SELECT max(price)FROM Printer)
 ---11---
-Select avg(speed ) from Pc
+SELECT avg(speed ) FROM Pc
 ---12---
-Select avg(speed) from Laptop where price >1000
+SELECT avg(speed) FROM Laptop WHERE price >1000
 ---13---
-select avg(speed) from Product join Pc on Product.model=Pc.model where maker ='A'
+SELECT avg(speed) FROM Product JOIN Pc ON Product.model=Pc.model WHERE maker ='A'
 ---14---
-select Classes.class,name,country from Classes join Ships on Classes.class=Ships.class where numGuns>=10
+SELECT Classes.class,name,country FROM Classes JOIN Ships ON Classes.class=Ships.class WHERE numGuns>=10
 ---15---
-Select hd from Pc group by hd having count(*)>=2
+SELECT hd FROM Pc GROUP BY hd HAVING count(*)>=2
 
